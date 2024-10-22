9e80069324a18421051784e4e059edf19ae72b2d
JEFERSON  LUIZ DA SILVA <0220832311038@fatectq.local>
2024-10-22 12:26:28 -0300

    atualizado

--- src/app/dao/ImovelDao.java
+++ src/app/dao/ImovelDao.java
@@ -5,6 +5,8 @@ import app.singleton.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImovelDao {
     private Connection connection;
@@ -12,12 +14,13 @@ public class ImovelDao {
     public ImovelDao() {
         this.connection = Conexao.getInstancia().getConexao();
     }
+
     public String salvar(Imovel imovel) {
-        String sql = "INSERT INTO imovel (rua, numero, tamanho, valor) "+
-        "VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO imovel (rua, numero, tamanho, valor) " +
+                "VALUES (?, ?, ?, ?)";
 
         try {
-            PreparedStatement ps= this.connection.prepareStatement(sql);
+            PreparedStatement ps = this.connection.prepareStatement(sql);
             ps.setString(1, imovel.getRua());
             ps.setString(2, imovel.getNumero());
             ps.setDouble(3, imovel.getTamanho());
@@ -28,11 +31,33 @@ public class ImovelDao {
 
             return "Imóvel Salvo";
 
-
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             return "Houve um erros";
         }
+    }
+
+    public List<Imovel> listar() {
+        List<Imovel> listaImoveis = new ArrayList<>();
+        String sql = "SELECT*FROM imovel";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Imovel imovel = new Imovel();
+                imovel.setCodigo(rs.getint("codigo"));
+                imovel.setRua(rs.getString("rua"));
+                imovel.setNumero(rs.getString("numero"));
+                imovel.setTamanho(rs.getDouble("Tamanho"));
+                imovel.setValor(rs.getDouble("valor"));
+                listaImoveis.add(imovel);
+            }
+            ps.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return listaImoveis;
     }
 }
