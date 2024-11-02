df73ff32d7dd716ca7167be92663febc51939c49
Jeferson <jhefersonlhuiz@gmail.com>
2024-11-02 17:03:34 -0300

    incluido banco dados

--- dbds2imovel.sql
+++ dbds2imovel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Out-2024 às 16:47
+-- Tempo de geração: 02-Nov-2024 às 21:01
 -- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.0.30
+-- versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `imovel` (
   `rua` varchar(100) DEFAULT NULL,
   `numero` varchar(10) DEFAULT NULL,
   `tamanho` decimal(10,3) DEFAULT NULL,
-  `valor` decimal(10,2) DEFAULT NULL
+  `valor` decimal(10,6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 --
@@ -40,8 +40,16 @@ CREATE TABLE `imovel` (
 --
 
 INSERT INTO `imovel` (`codigo`, `rua`, `numero`, `tamanho`, `valor`) VALUES
-(1, 'fioravante', '7', 100.000, 10000.00),
-(2, 'adail nunes', '5', 500.000, 50000.00);
+(3, 'Rua das flores', '1', 300.000, 250.000000),
+(4, 'Avenida do sol', '10', 250.000, 200.000000),
+(5, 'Rua Travessa do lago', '40', 400.000, 350.000000),
+(6, 'Rua alameda aguas', '555', 450.000, 375.000000),
+(7, 'Estrada dos ventos', '689', 600.000, 500.000000),
+(8, 'Rua dos horizontes', '456', 350.000, 280.000000),
+(9, 'Rua beco da amizade', '236', 280.000, 240.000000),
+(10, 'Avenida da paz', '478', 700.000, 350.000000),
+(11, 'Rua das andorinhas', '856', 560.000, 1000.000000),
+(12, 'Rua passadadina', '523', 350.000, 550.000000);
 
 --
 -- Índices para tabelas despejadas
@@ -61,7 +69,7 @@ ALTER TABLE `imovel`
 -- AUTO_INCREMENT de tabela `imovel`
 --
 ALTER TABLE `imovel`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
